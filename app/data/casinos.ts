4a8dfed11d6a447477fc45b5618a2cd75680f193
Javid Mamadli <javid.mammadli99@gmail.com>
2026-02-01 14:55:39 +0400

    ..

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -30,41 +30,41 @@ const topBadges = ["Best Bonus", "Top Pick", "Trending Now"];
 ///
 // Raw casino data without id, rating, and badge
 const casinosRawData: CasinoRawData[] = [
-  {
-    name: "Lizaro",
-    logo: "/lizaro.png",
-    bonus: "350% up to £680 + 200 FREE SPINS",
-    url: "https://any-site1.com/hhbHzRbC",
-    isMobile: true
-  },
-  {
-    name: "HarryCasino",
-    logo: "/harry.svg",
-    bonus: "£1000 BONUS + 100 Free Spins",
-    url: "https://any-site1.com/mSFVwGrp",
-    isMobile:true
-  },
-  {
-    name: "LuckyWave",
-    logo: "/luckywave.svg",
-    bonus: "100% UP TO 2,000 €/£ + 200 FREE SPINS",
-    url: "https://any-site1.com/m5Cn7JWD",
-    isMobile: true
-  },
-  {
-    name: "LuckyTwice",
-    logo: "/luckytwice.svg",
-    bonus: "100% up to £500 + 250 FREE SPINS",
-    url: "https://any-site1.com/pjskyjLq",
-    isMobile:true
-  },
-  {
-    name: "Agent No Wager",
-    logo: "/agentnowager.svg",
-    bonus: "Up to 90 FREE SPINS + 45% Without Wager!",
-    url: "https://any-site1.com/ht2pbwVK",
-    isMobile: true
-  },
+  // {
+  //   name: "Lizaro",
+  //   logo: "/lizaro.png",
+  //   bonus: "350% up to £680 + 200 FREE SPINS",
+  //   url: "https://any-site1.com/hhbHzRbC",
+  //   isMobile: true
+  // },
+  // {
+  //   name: "HarryCasino",
+  //   logo: "/harry.svg",
+  //   bonus: "£1000 BONUS + 100 Free Spins",
+  //   url: "https://any-site1.com/mSFVwGrp",
+  //   isMobile:true
+  // },
+  // {
+  //   name: "LuckyWave",
+  //   logo: "/luckywave.svg",
+  //   bonus: "100% UP TO 2,000 €/£ + 200 FREE SPINS",
+  //   url: "https://any-site1.com/m5Cn7JWD",
+  //   isMobile: true
+  // },
+  // {
+  //   name: "LuckyTwice",
+  //   logo: "/luckytwice.svg",
+  //   bonus: "100% up to £500 + 250 FREE SPINS",
+  //   url: "https://any-site1.com/pjskyjLq",
+  //   isMobile:true
+  // },
+  // {
+  //   name: "Agent No Wager",
+  //   logo: "/agentnowager.svg",
+  //   bonus: "Up to 90 FREE SPINS + 45% Without Wager!",
+  //   url: "https://any-site1.com/ht2pbwVK",
+  //   isMobile: true
+  // },
 
 
 
