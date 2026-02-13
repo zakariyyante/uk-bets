81bc0c835f27fff98e3de1dd90eea7555406f5f2
Sabuhi Tapdigli <sabuhitapdigli@Sabuhis-MacBook-Pro.local>
2026-02-13 14:49:45 +0400

    new

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -41,23 +41,23 @@ const casinosRawData: CasinoRawData[] = [
     votes: 6440,
     isMobile: true
   },
-  {
-    name: "Lizaro",
-    logo: "/lizaro.png",
-    // bonus: "350% up to £680 + 200 FREE SPINS",
-    bonus: "200% up to £1000 + 250 FREE SPINS",
-    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=lizaro",
-    votes: 5872,
-    isMobile: true
-  },
-  {
-    name: "SpinShark",
-    logo: "/spinshark.svg",
-    bonus: "£900 BONUS + 150 Free Spins",
-    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=spinshark",
-    votes: 4143,
-    isMobile: true
-  },
+  // {
+  //   name: "Lizaro",
+  //   logo: "/lizaro.png",
+  //   // bonus: "350% up to £680 + 200 FREE SPINS",
+  //   bonus: "200% up to £1000 + 250 FREE SPINS",
+  //   url: "https://topbritcasinos.com/B7W2b6jr?utm_target=lizaro",
+  //   votes: 5872,
+  //   isMobile: true
+  // },
+  // {
+  //   name: "SpinShark",
+  //   logo: "/spinshark.svg",
+  //   bonus: "£900 BONUS + 150 Free Spins",
+  //   url: "https://topbritcasinos.com/B7W2b6jr?utm_target=spinshark",
+  //   votes: 4143,
+  //   isMobile: true
+  // },
   // {
   //   name: "HarryCasino",
   //   logo: "/harry.svg",
