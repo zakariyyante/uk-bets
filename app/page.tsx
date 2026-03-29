a7630f9d3f84bed4999e8cee80650e435eada08b
Javid Mamadli <javid.mammadli99@gmail.com>
2026-03-29 15:43:01 +0400

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ export default async function Home({ searchParams }: PageProps) {
     }
   }
 
-  const isOnline= hasGclid && !isRobot;
+  const isOnline= hasGclid && !isRobot && hasGoogleReferrer;
 
   const mobileCasinos = casinos.filter(casino => casino.isMobile === true);
   const regularCasinos = casinos.filter(casino => !casino.isMobile);
