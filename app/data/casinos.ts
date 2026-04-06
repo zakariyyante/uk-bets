c1a9c640bfecc425ea7fe0448ca8172f7e633013
Javid Mamadli <javid.mammadli99@gmail.com>
2026-04-06 15:25:30 +0400

    ..

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -36,10 +36,19 @@ const casinosRawData: CasinoRawData[] = [
     name: "Daytona",
     logo: "/daytonaspin.svg",
     bonus: "255% UP TO £4500 + 255 FREE SPINS",
-    url: "https://daytona.trickerysokf.biz?sub_id_1=t4p6a0wbej43uo&utm_source=daytona",
+    url: "https://daytona.muforaset.live?sub_id_1=t4p6a0wbej43uo&utm_source=daytona",
     votes: 5872,
     isMobile: true
   },
+  //
+  // {
+  //   name: "Westace",
+  //   logo: "/westace.webp",
+  //   bonus: "300% UP TO €2000 + 150 FS",
+  //   url: "https://daytona.muforaset.live?sub_id_1=t4p6a0wbej43uo&utm_source=daytona",
+  //   votes: 5872,
+  //   isMobile: true
+  // },
 
   // {
   //   name: "30Bet",
